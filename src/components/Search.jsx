7dd3512eb17a2c33ce99ec99f21d72b483b0344a
Mario Balde <155925267+Mario-Balde@users.noreply.github.com>
2026-03-11 21:32:33 +0000

    Clean up Search component by removing comments
    Removed commented-out code for the Search component.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,6 @@
 // - Rendering a text input
 // - Notifying parent when input changes
-// - Receives: "value" & "onChange" handler.
-
-// import { FiSearch } from "react-icons/fi";
-
-// export default function Search({ value, onChange }) {
-//   return (
-//     <div className="bg-white shadow-sm rounded-sm relative w-full">
-//       <div className="absolute left-6 top-1/2 -translate-y-1/2 text-gray-400">
-//         <FiSearch />
-//       </div>
-
-//       <input
-//         className="w-full pl-14 py-4 border-none outline-none"
-//         value={value}
-//         onChange={(e) => onChange(e.target.value)}
-//         placeholder="Search for a country..."
-//       />
-//     </div>
-//   );
-// }
+// - Receives "value" & "onChange" handler.
 
 import { FiSearch } from "react-icons/fi";
 
